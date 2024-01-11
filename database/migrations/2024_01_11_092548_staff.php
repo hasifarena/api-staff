b24c941d84f37999d9652d5ce6aa44e0757d7730
mohdhasif <hasif.arenaauctioneers@gmail.com>
2024-01-11 17:45:50 +0800

    update

---
+++ database/migrations/2024_01_11_092548_staff.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Staff extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id();
+            $table->string('staffFullname');
+            $table->string('staffEmail')->unique();
+            $table->timestamp('staffVerifiedDate')->nullable();
+            $table->string('staffPassword');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
